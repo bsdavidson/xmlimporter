c4a1a87ae18c5126980cd44f4f17b23fb153aab7
Brian Davidson <bsdavidson@gmail.com>
2014-05-30 16:13:19 -0500

    removed args from fcuntion parameters

--- parsexml.py
+++ parsexml.py
@@ -12,22 +12,25 @@ def parse_args():
     parser.add_argument('-b', '--targettable', help='Target Table')
     return parser.parse_args()
 
-def get_xml(args):
-    input_xml = args.inputxml
+
+def get_xml(input_xml):
     xml_file = open(input_xml, 'r')
     xml = minidom.parse(xml_file)
     xml_file.close()
     return xml
 
+
 def get_rows(xml):
     rows = xml.documentElement.getElementsByTagName('row')
     return rows
+
 
 def get_field_name(rows):
     field = rows[0].getElementsByTagName('field')
     a = field[1].attributes["name"]
     field_name = a.value
     return field_name
+
 
 def get_many_args(rows):
     many_args = []
@@ -39,15 +42,13 @@ def get_many_args(rows):
         many_args.append(args)
     return many_args
 
-def get_db_conn(args):
-    target_db = args.targetdb
-    db_user = args.dbuser
-    db_pass = args.dbpassword
-    host = args.host
+
+def get_db_conn(target_db, db_user, db_pass, host):
     db_conn = pymysql.connect(user=db_user, passwd=db_pass, host=host,db=target_db,
                               unix_socket="/tmp/mysql.sock", charset='utf8')
     cursor = db_conn.cursor()
     return cursor
+
 
 def update_db(db_conn, target_table, field_name, many_args):
     query = ('UPDATE ' + target_table + ' SET ' + 
@@ -56,16 +57,23 @@ def update_db(db_conn, target_table, field_name, many_args):
     db_conn.execute('COMMIT')
     db_conn.close()
 
+
 def main():
     args = parse_args()
-    xml = get_xml(args)
+    xml = get_xml(args.inputxml)
     rows = get_rows(xml)
     field_name = get_field_name(rows)
     many_args = get_many_args(rows)
-    db_conn = get_db_conn(args)
+    db_conn = get_db_conn(args.targetdb, args.dbuser, args.dbpassword, args.host)
     target_table = args.targettable
     update_db(db_conn, target_table, field_name, many_args)
     db_conn.close()    
 
+
 if __name__ == "__main__":
     main()
+
+
+def test_rows():
+    args = parse_args()
+    xml = get_xml(args)
