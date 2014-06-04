ff36743b87222532eff0f45a15c7ac75ac5fafed
Brian Davidson <bsdavidson@gmail.com>
2014-06-04 14:58:07 -0500

    Got 4 tests now.. database can go test itself..

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,3 +4,4 @@ import sys
 test_dir = os.path.dirname(__file__)
 root_dir = os.path.abspath(os.path.join(test_dir,'..'))
 sys.path.insert(0, root_dir)
+os.chdir(test_dir)
--- tests/test_parsexml.py
+++ tests/test_parsexml.py
@@ -1,4 +1,4 @@
-from parsexml import get_rows, get_xml
+from parsexml import get_rows, get_xml, get_many_args
 
 
 def test_xml_file():
@@ -9,3 +9,26 @@ def test_xml_rows():
     xml_data = get_xml('test.xml')
     rows = get_rows(xml_data)
     assert len(rows) == 2
+
+
+def get_field_name(rows):
+    field = rows[0].getElementsByTagName('field')
+    a = field[1].attributes["name"]
+    field_name = a.value
+    return field_name
+
+
+
+def test_get_fieldnames():
+    xml_data = get_xml('test.xml')
+    rows = get_rows(xml_data)
+    field_name = get_field_name(rows)
+    assert field_name == "text_test"
+
+
+def test_get_args():
+    xml_data = get_xml('test.xml')
+    rows = get_rows(xml_data)
+    args = get_many_args(rows)
+    assert len(args) == 2
+
